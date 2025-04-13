61d460077de84457a11407538ddd58e4265bdac6
Andresf-Asprilla <andresf.asprilla1414@gmail.com>
2025-04-13 18:44:50 -0500

    correcion descarga modelos

--- App_radilogia/download_model_colab.py
+++ App_radilogia/download_model_colab.py
@@ -6,7 +6,7 @@ os.makedirs("model", exist_ok=True)
 
 models = {
     "segmentation_coa.pt": "1-m5lLKEiwziJO8E5_x80-8c25EAW7F1D",
-    "pretrained_segmentation_spleen.pt": "10Ik1x_PQH45ysGxMkPpYwk09IecJix2"
+    "pretrained_segmentation_spleen.pt": "10Ik1x_PQH45ysGxMkPpYwk09IecJix2L"
 }
 for filename, file_id in models.items():
     url = f"https://drive.google.com/uc?id={file_id}"
