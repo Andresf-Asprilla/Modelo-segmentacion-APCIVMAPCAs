561ab8b60095950d4e3d49a6d1faf7a83ed26a36
Andresf-Asprilla <andresf.asprilla1414@gmail.com>
2025-05-19 16:58:54 -0500

    actualizacion cargar modelo

--- nn-Unet/descarga_modelo.py
+++ nn-Unet/descarga_modelo.py
@@ -3,21 +3,51 @@ import zipfile
 import gdown
 
 # Detectar automáticamente el directorio base donde se ejecuta el usuario
-base_dir = os.getcwd()
+mount_dir= os.getcwd()
 
-# Definir rutas a partir del directorio actual
-raw_data = os.path.join(base_dir, "nnUNet_raw")
-preprocessed = os.path.join(base_dir, "nnUNet_preprocessed")
-results = os.path.join(base_dir, "nnUNet_results")
+def make_if_dont_exist(folder_path,overwrite=False):
+    """
+    creates a folder if it does not exists
+    input:
+    folder_path : relative path of the folder which needs to be created
+    over_write :(default: False) if True overwrite the existing folder
+    """
+    if os.path.exists(folder_path):
 
-# Establecer variables de entorno
-os.environ["NNUNET_RAW_DATA"] = raw_data
-os.environ["NNUNET_PREPROCESSED"] = preprocessed
-os.environ["NNUNET_RESULTS"] = results
+        if not overwrite:
+            print(f"{folder_path} exists.")
+        else:
+            print(f"{folder_path} overwritten")
+            shutil.rmtree(folder_path)
+            os.makedirs(folder_path)
 
-# Crear carpetas necesarias
-for path in [raw_data, preprocessed, results]:
-    os.makedirs(path, exist_ok=True)
+    else:
+      os.makedirs(folder_path)
+      print(f"{folder_path} created!")
+
+      # Maybe move path of preprocessed data directly on content - this may be signifcantely faster!
+print("Current Working Directory {}".format(os.getcwd()))
+path_dict = {
+    "nnUNet_raw" : os.path.join(mount_dir, "nnUNet_raw"),
+    "nnUNet_preprocessed" : os.path.join(mount_dir, "nnUNet_preprocessed"), # 1 experiment: 1 epoch took 112s
+    "nnUNet_results" : os.path.join(mount_dir, "nnUNet_results"),
+    "RAW_DATA_PATH" : os.path.join(mount_dir, "RawData"), # This is used here only for convenience (not necessary for nnU-Net)!
+}
+
+# Write paths to environment variables
+for env_var, path in path_dict.items():
+  os.environ[env_var] = path
+
+# Check whether all environment variables are set correct!
+for env_var, path in path_dict.items():
+  if os.getenv(env_var) != path:
+    print("Error:")
+    print("Environment Variable {} is not set correctly!".format(env_var))
+    print("Should be {}".format(path))
+    print("Variable is {}".format(os.getenv(env_var)))
+  make_if_dont_exist(path, overwrite=False)
+
+print("If No Error Occured Continue Forward. =)")
 
 # Descargar el modelo desde Google Drive
 url = "https://drive.google.com/uc?id=1NYfEutP5l01w-7YGXQzP5Boo5MNzw7CO"
@@ -27,7 +57,7 @@ gdown.download(url, zip_path, quiet=False)
 # Extraer el modelo en la carpeta de resultados
 def extract_model_to_results(zip_path):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(results)
+        zip_ref.extractall(os.path.join(os.getenv("nnUNet_results")))
 
 extract_model_to_results(zip_path)
 os.remove(zip_path)
